a3d2de8c15e2597aebdd0132d01a79741729dec5
Arkadiusz Ćwik <anarkadiusz.cwik@gmail.com>
2023-06-27 11:28:25 +0200

    Basics of Redux part2

--- redux-demo.js
+++ redux-demo.js
@@ -1,17 +1,19 @@
-const redux = require('redux');
+const redux = require("redux");
 
-const counterReduser = (state={counter:0},action)=>{
-return {
-  counter:state.counter +1 ,
-
-};
+const counterReduser = (state = { counter: 0 }, action) => {
+	if (action.type === "increment") {
+		return {
+			counter: state.counter + 1,
+		};
+	} 
+  return state
 };
 const store = redux.createStore(counterReduser);
 
-const counterSubscriber = ()=>{
-  const latestState = store.getState();
-  console.log(latestState);
+const counterSubscriber = () => {
+	const latestState = store.getState();
+	console.log(latestState);
 };
 
 store.subscribe(counterSubscriber);
-store.dispatch({type:'increment'});
+store.dispatch({ type: "increment" });
